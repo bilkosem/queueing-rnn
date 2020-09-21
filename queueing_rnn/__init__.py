048cfd80e417cd70c913c83c8e3cfde5e7e974a2
Bilgehan Kösem <bilkos92@gmail.com>
2020-09-22 00:05:16 +0300

    pip package has been created

---
+++ queueing_rnn/__init__.py
@@ -0,0 +1,1 @@
+from .QRNN import QRNN
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="queueing_rnn",
+    version="1.0.3",
+    author="bilkosem",
+    author_email="bilkos92@gmail.com",
+    description="Queueing Recurrent Neural Network",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bilkosem/QRNN-Queueing-Recurrent-Neural-Network",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
